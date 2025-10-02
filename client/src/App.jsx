f29a935136e498d8fc186421ce469247544d7ba5
Venna Sri Anjani <srianjani.9704@gmail.com>
2025-10-02 11:07:22 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,6 @@ import Home from './pages/Home'
 import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
 import WriteArticle from './pages/WriteArticle'
-import BlogTitles from './pages/BlogTitles'
-import GenerateImages from './pages/GenerateImages'
-import RemoveBackground from './pages/RemoveBackground'
-import RemoveObject from './pages/RemoveObject'
-import ReviewResume from './pages/ReviewResume'
 import Community from './pages/Community'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
@@ -28,8 +23,7 @@ const App = () => {
           <Route path='blog-titles' element={<BlogTitles/>}/>
           <Route path='generate-images' element={<GenerateImages/>}/>
           <Route path='remove-background' element={<RemoveBackground/>}/>
-          <Route path='remove-object' element={<RemoveObject/>}/>
-          <Route path='review-resume' element={<ReviewResume/>}/>
+
           <Route path='community' element={<Community/>}/>
 
         </Route>
